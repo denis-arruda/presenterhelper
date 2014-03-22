f940423f4c3e2d4d90a805cd1a55e43fc5aaba8c
denis-arruda <denis.arruda@gmail.com>
2014-03-21 23:00:38 -0300

    Geracao do slide de titulo

--- controllers/default.py
+++ controllers/default.py
@@ -12,25 +12,80 @@
 
 import base64
 
+fileheader = ("<RVPresentationDocument height=\"720\" width=\"1280\" versionNumber=\"500\" docType=\"0\" creatorCode=\"0000000000\" lastDateUsed=\"2014-03-12T18:16:36\" usedCount=\"0\" category=\"M\xc3\xbasica\" resourcesDirectory=\"\" backgroundColor=\"1 0.5 0 1\" drawingBackgroundColor=\"0\" notes=\"\" artist=\"\" author=\"\" album=\"\" CCLIDisplay=\"0\" CCLIArtistCredits=\"\" CCLISongTitle=\"\" CCLIPublisher=\"\" CCLICopyrightInfo=\"\" CCLILicenseNumber=\"\" chordChartPath=\"\">"
+    "<timeline timeOffSet=\"0\" selectedMediaTrackIndex=\"0\" unitOfMeasure=\"60\" duration=\"0\" loop=\"0\">"
+    "<timeCues containerClass=\"NSMutableArray\"></timeCues>"
+    "<mediaTracks containerClass=\"NSMutableArray\"></mediaTracks>"
+    "</timeline>"
+    "<bibleReference containerClass=\"NSMutableDictionary\"></bibleReference>"
+    "<_-RVProTransitionObject-_transitionObject transitionType=\"-1\" transitionDuration=\"1\" motionEnabled=\"0\" motionDuration=\"20\" motionSpeed=\"100\"></_-RVProTransitionObject-_transitionObject>"
+    "<groups containerClass=\"NSMutableArray\">"
+    "<RVSlideGrouping name=\"galactic-fireworks-stars\" uuid=\"FF45CF72-C3AF-4DD0-9C52-ADF428C4D6D9\" color=\"0 0 1 1\" serialization-array-index=\"0\">"
+    "<slides containerClass=\"NSMutableArray\">"
+    "<RVDisplaySlide backgroundColor=\"0 0 0 0\" enabled=\"1\" highlightColor=\"\" hotKey=\"\" label=\"\" notes=\"\" slideType=\"1\" sort_index=\"0\" UUID=\"1AAC2C18-292F-42B1-8EAF-518C80ADC19C\" drawingBackgroundColor=\"0\" chordChartPath=\"\" serialization-array-index=\"0\">"
+    "<cues containerClass=\"NSMutableArray\">"
+    "<RVMediaCue displayName=\"galactic-fireworks-stars\" delayTime=\"0\" timeStamp=\"0\" enabled=\"1\" UUID=\"5FA2E744-C870-41B7-8725-7819E6724C41\" parentUUID=\"5FA2E744-C870-41B7-8725-7819E6724C41\" elementClassName=\"RVVideoElement\" behavior=\"1\" alignment=\"4\" serialization-array-index=\"0\">"
+	"<element displayDelay=\"0\" displayName=\"galactic-fireworks-stars\" locked=\"0\" persistent=\"0\" typeID=\"0\" fromTemplate=\"0\" bezelRadius=\"0\" drawingFill=\"0\" drawingShadow=\"0\" drawingStroke=\"0\" fillColor=\"1 1 1 1\" rotation=\"0\" source=\"file://localhost/Users/m3cpcp/Documents/Liturgia/Biblioteca%20de%20vi%CC%81deos/Vi%CC%81deos%20de%20Fundo/galactic-fireworks-stars.mov\" flippedHorizontally=\"0\" flippedVertically=\"0\" scaleFactor=\"1\" serializedImageOffset=\"0.000000@0.000000\" serializedFilters=\"YnBsaXN0MDDUAQIDBAUIFhdUJHRvcFgkb2JqZWN0c1gkdmVyc2lvblkkYXJjaGl2ZXLRBgdUcm9vdIABowkKD1UkbnVsbNILDA0OViRjbGFzc1pOUy5vYmplY3RzgAKg0hAREhNYJGNsYXNzZXNaJGNsYXNzbmFtZaMTFBVeTlNNdXRhYmxlQXJyYXlXTlNBcnJheVhOU09iamVjdBIAAYagXxAPTlNLZXllZEFyY2hpdmVyCBEWHygyNTo8QEZLUl1fYGVueX2MlJ2iAAAAAAAAAQEAAAAAAAAAGAAAAAAAAAAAAAAAAAAAALQ=\" scaleBehavior=\"1\" brightness=\"0\" contrast=\"1\" saturation=\"1\" hue=\"0\" manufactureURL=\"\" manufactureName=\"\" format=\"H.264\" enableColorFilter=\"0\" colorFilter=\"1 0 0 1\" enableBlur=\"0\" blurRadius=\"0\" enableEdgeBlur=\"0\" edgeBlurRadius=\"0\" edgeBlurArea=\"0\" enableSepia=\"0\" enableColorInvert=\"0\" enableGrayInvert=\"0\" enableHeatSignature=\"0\" audioVolume=\"1\" inPoint=\"0\" outPoint=\"720720\" playRate=\"1\" playbackBehavior=\"1\" timeScale=\"24000\" endPoint=\"720720\">"
+	"<_-RVRect3D-_position x=\"0\" y=\"0\" z=\"0\" width=\"1280\" height=\"720\"></_-RVRect3D-_position>"
+	"<_-D-_serializedShadow containerClass=\"NSMutableDictionary\">"
+	"<NSMutableString serialization-native-value=\"{5, -5}\" serialization-dictionary-key=\"shadowOffset\"></NSMutableString>"
+	"<NSNumber serialization-native-value=\"0\" serialization-dictionary-key=\"shadowBlurRadius\"></NSNumber>"
+	"<NSColor serialization-native-value=\"0 0 0 0.3333333432674408\" serialization-dictionary-key=\"shadowColor\"></NSColor>"
+	"</_-D-_serializedShadow>"
+	"<stroke containerClass=\"NSMutableDictionary\">"
+	"<NSColor serialization-native-value=\"0 0 0 1\" serialization-dictionary-key=\"RVShapeElementStrokeColorKey\"></NSColor>"
+	"<NSNumber serialization-native-value=\"1\" serialization-dictionary-key=\"RVShapeElementStrokeWidthKey\"></NSNumber>"
+	"</stroke>"
+	"</element>"
+	"<_-RVProTransitionObject-_transitionObject></_-RVProTransitionObject-_transitionObject>"
+	"</RVMediaCue>"
+    "</cues>"
+    "<displayElements containerClass=\"NSMutableArray\">")
+  
+titlepart1 = ("<RVTextElement displayDelay=\"0\" displayName=\"\" locked=\"0\" persistent=\"0\" typeID=\"0\" fromTemplate=\"0\" bezelRadius=\"0\" drawingFill=\"0\" drawingShadow=\"1\" drawingStroke=\"0\" fillColor=\"0 0 0 1\" rotation=\"0\" source=\"\" adjustsHeightToFit=\"0\" verticalAlignment=\"1\" RTFData=\"")
+titlepart2 = ("\" revealType=\"0\" serialization-array-index=\"0\">"
+    "<_-RVRect3D-_position x=\"40\" y=\"40\" z=\"1\" width=\"1200\" height=\"160\"></_-RVRect3D-_position>"
+    "<_-D-_serializedShadow containerClass=\"NSMutableDictionary\">"
+	"<NSMutableString serialization-native-value=\"{4.3301301, -2.5}\" serialization-dictionary-key=\"shadowOffset\"></NSMutableString>"
+	"<NSNumber serialization-native-value=\"5\" serialization-dictionary-key=\"shadowBlurRadius\"></NSNumber>"
+	"<NSColor serialization-native-value=\"0 0 0 0.75\" serialization-dictionary-key=\"shadowColor\"></NSColor>"
+	"</_-D-_serializedShadow>"
+	"<stroke containerClass=\"NSMutableDictionary\">"
+	"<NSColor serialization-native-value=\"0 0 0 1\" serialization-dictionary-key=\"RVShapeElementStrokeColorKey\"></NSColor>"
+	"<NSNumber serialization-native-value=\"1\" serialization-dictionary-key=\"RVShapeElementStrokeWidthKey\"></NSNumber>"
+	"</stroke>"
+    "</RVTextElement>")
+  
+filefooter = ("</displayElements>"
+    "<_-RVProTransitionObject-_transitionObject transitionType=\"-1\" transitionDuration=\"1\" motionEnabled=\"0\" motionDuration=\"20\" motionSpeed=\"100\"></_-RVProTransitionObject-_transitionObject>"
+    "</RVDisplaySlide>"
+    "</slides>"
+    "</RVSlideGrouping>"
+    "</groups>"
+    "<arrangements containerClass=\"NSMutableArray\"></arrangements>"
+    "</RVPresentationDocument>")
+
 def index():
     return dict(message="Hello from MyApp")
 
 def songform():
     if request.vars.title:
-		# session.title = maketitledata(request.vars.title, request.vars.author)
-        # redirect(URL('songfile'))
-        response.headers['Content-Type'] = 'text/xml'
-        attachment = 'attachment;filename=' + request.vars.title + '.pro5'
-        response.headers['Content-Disposition'] = attachment
-        content = maketitledata(request.vars.title, request.vars.author)
-        raise HTTP(200,str(content),
-                   **{'Content-Type':'text/xml',
-                      'Content-Disposition':attachment + ';'})
+        session.title = request.vars.title
+        session.author = request.vars.author
         redirect(URL('songfile'))
     return dict()
 	
 def songfile():
-	return dict(message="Hello from MyApp")
+	return dict(message="O arquivo está sendo gerado...")
+	
+def generate():
+    response.headers['Content-Type'] = 'text/xml'
+    attachment = 'attachment;filename=' + session.title + '.pro5'
+    response.headers['Content-Disposition'] = attachment
+    content = fileheader + titlepart1 + maketitledata(session.title, session.author) + titlepart2 + filefooter
+    raise HTTP(200,str(content),
+               **{'Content-Type':'text/xml',
+                  'Content-Disposition':attachment + ';'})
 
 def maketitledata( title, author):
     return base64.b64encode(formattitle(title,author))
